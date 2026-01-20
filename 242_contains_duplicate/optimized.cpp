515cf2caf35eeb5b39ba2aa8e4b26bbeae6bd59a
Harshad Gore <Harshad.Gore@amdocs.com>
2026-01-20 18:52:18 +0530

    242

---
+++ 242_contains_duplicate/optimized.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+      unordered_set<int> us;
+      for (int i=0;i<nums.size();i++)
+      {
+          if(us.find(nums[i])!=us.end())
+          {
+            return true;
+          }
+
+          us.insert(nums[i]);
+      }
+      return false;
+    }
+};
+
+//TC-> O(n)
+//SC-> O(n)
